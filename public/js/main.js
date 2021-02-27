e86ec4d688978d9c2854745b3aad45ad24bb05ed
sheila <devsheilawambui@gmail.com>
2021-02-27 22:56:30 +0300

    Creating outline of required files

---
+++ public/js/main.js
@@ -0,0 +1,20 @@
+
+$(document).ready(function(){
+ // Toggle Effect on what we do section
+   $('.what-icon').click(function(e){
+       let whatIcon = e.currentTarget;
+       let whatPara = e.currentTarget.nextElementSibling.childNodes[3];
+       $(whatPara).toggle(function(){
+
+        $(whatIcon).hide();
+        $(whatPara).click(function(){
+            $(whatIcon).show()
+            $(whatPara).hide();
+        })
+       })
+   })
+
+   $()
+    
+    
+})
